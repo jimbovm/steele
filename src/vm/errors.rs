748adc54b7c917b5fc7171ee4aa6ea049faefd22
James Brierley <jmb8710@gmail.com>
2025-09-02 06:49:01 +0100

    use thiserror and add interpreter errors

---
+++ src/vm/errors.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum InterpreterError {
+	#[error("stack overflow")]
+	StackOverflow,
+	#[error("stack underflow")]
+	StackUnderflow,
+	#[error("premature end of code at {0}")]
+	EndOfCode(u32),
+	#[error("jump out of bounds to {0}, code max byte address is {1}")]
+	JumpOutOfBounds(u32, usize),
+}
